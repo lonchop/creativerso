43f90b0f8addfc892256fb455a0f35085b6bfe30
Orangel Gonzalez <goranjose16@gmail.com>
2022-03-21 09:20:27 -0400

    commit 6.5

--- ignorados/loading.js
+++
@@ -1,8 +0,0 @@
-const loader = () => {
-  document.querySelector(".loader-container").classList.add("fade-out");
-};
-
-const fadeOut = () => {
-  setInterval(loader, 3000);
-};
-window.onload = fadeOut();
